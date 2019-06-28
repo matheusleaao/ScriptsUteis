b942888baf3e6b30bd8656b6322b0cc1b977828d
M. Leão da C. Pereira <mathegiov@hotmail.com>
2019-06-28 13:07:02 -0300

    Exercicio3
    Escreva um programa que receba dois números e um sinal, 
    e faça a operação matemática definida pelo sinal.

---
+++ Exercicio3.py
@@ -0,0 +1,29 @@
+"""Escreva um programa que receba dois números e um sinal, 
+e faça a operação matemática definida pelo sinal."""
+
+cont = 1
+print("Entre com o "+str(cont)+"º número")
+numUm = float(input())
+
+cont+=cont
+
+print("Entre com o "+str(cont)+"º número")
+numDois = float(input())
+
+print("Qual operação deseja que seja feita?")
+operacao = input()
+
+if operacao == '*':
+    resultado = numUm * numDois
+    print("O resultado de "+str(numUm)+" "+operacao+" "+str(numDois)+" é de "+str(resultado)+"")
+elif operacao == '/':
+    resultado = numUm / numDois
+    print("O resultado de "+str(numUm)+" "+operacao+" "+str(numDois)+" é de "+str(resultado)+"")
+elif operacao == '+':
+    resultado = numUm + numDois
+    print("O resultado de "+str(numUm)+" "+operacao+" "+str(numDois)+" é de "+str(resultado)+"")
+elif operacao == '-':
+    resultado = numUm - numDois
+    print("O resultado de "+str(numUm)+" "+operacao+" "+str(numDois)+" é de "+str(resultado)+"")
+else:
+    print("Operação inválida.")
