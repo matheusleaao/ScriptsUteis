9c05a8970adbd8e823b3120f1e3d9f208c1d6d3d
M. Leão da C. Pereira <mathegiov@hotmail.com>
2019-06-28 10:24:40 -0300

    Exercício 1
    Faça um programa que receba a idade do usuário e diga se ele é maior ou menor de idade.

---
+++ Exercicio1.py
@@ -0,0 +1,12 @@
+"""Faça um programa que receba 
+a idade do usuário e diga se ele 
+é maior ou menor de idade.  """
+
+print("Digite sua idade: ")
+
+idade = int(input())
+
+if idade >= 18:
+    print("Você é maior de idade! :)")
+else:    
+    print("Você é menor de idade! :( ")
