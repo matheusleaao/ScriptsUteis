0db6cbdcd4c48db64d4a19f0c79a2632bb11ba71
M. Leão da C. Pereira <mathegiov@hotmail.com>
2019-06-28 09:48:09 -0300

    Concatenação
    Utilizando range de string e +variavel+

---
+++ concatenacao.py
@@ -0,0 +1,36 @@
+'''
+
+                            Online Python Interpreter.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+
+
+# -*- coding:UTF-8 -*- #código de linguagem local, definir acentos nas palavras
+
+#dedicado ao aprendizado de concatenação
+
+a = "Matheus"
+b = "Leão"
+
+concatenação = a + " " + b 
+
+#mostrar o resultado normalmente
+print(concatenação)
+
+#mostrar concatenação em range
+print(concatenação[0])      #M
+print(concatenação[0:1])    #MA
+print(concatenação[0:2])    #MAT
+print(concatenação[0:3])    #MATH
+print(concatenação[0:4])    #MATHE
+print(concatenação[0:5])    #MATHEU
+print(concatenação[0:6])    #MATHEUS
+print(concatenação[0:7])    #MATHEUS_
+print(concatenação[0:8])    #MATHEUS_
+print(concatenação[0:9])    #MATHEUS L
+print(concatenação[0:10])   #MATHEUS LE
+print(concatenação[0:11])   #MATHEUS LEÃ
+print(concatenação[0:12])   #MATHEUS lEÃO
