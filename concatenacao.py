6d4d9fea6ec4a0cdb1203dba3fec82049fd29158
M. Leão da C. Pereira <mathegiov@hotmail.com>
2020-02-17 10:38:50 -0300

    Update concatenacao.py

--- concatenacao.py
+++ concatenacao.py
@@ -1,10 +1,7 @@
-'''
-
-                            Online Python Interpreter.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
+# Testes Python 
+#
+# Github : matheusLeaao  <https://github.com/matheusLeaao>
+# @author Matheus Leão <mathegiov@hotmail.com>
 
 
 
